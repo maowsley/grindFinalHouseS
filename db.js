ff70d272097838bdf0812cdba3adc709d9a2e3e6
Michelle Owsley <michowsley@gmail.com>
2022-02-12 13:31:14 -0500

    added heroku

--- db.js
+++ db.js
@@ -2,6 +2,12 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     ssl: process.env.ENVIRONMENT == 'production'
 });
 
