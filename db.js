a03197f009fe422d6cc8e62a3682958f52d2c324
Michelle Owsley <michowsley@gmail.com>
2022-02-19 17:52:44 -0500

    package

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
-const {Sequelize} = require('sequelize');
+const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    ssl: process.env.ENVIRONMENT === 'production'
-   
-    
+ 
+    ssl: process.env.ENVIRONMENT == 'production'
 });
-
 module.exports = sequelize;
