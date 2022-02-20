356a4bce9c4acb1a79a953057237a36011ecd85d
Michelle Owsley <michowsley@gmail.com>
2022-02-19 20:09:39 -0500

    sggl

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-   
+    dialect:'postgres',
+    dialectOptions:{
+        ssl: {
+            require:true,
+            rejectUnauthorized: false
+        }
+    },
+    ssl:process.env.ENVIRONMENT === 'production'
     
 });
 module.exports = sequelize;
